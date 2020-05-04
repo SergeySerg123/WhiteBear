300bf8c8186fe3172e13de9fa5e341dc6c5ba074
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-05 01:00:11 +0300

    Update RefreshDataArrow.js

--- src/components/Shop/RefreshDataArrow/RefreshDataArrow.js
+++ src/components/Shop/RefreshDataArrow/RefreshDataArrow.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
 import { Col } from 'reactstrap';
+import './RefreshDataArrow.scss';
 
 const RefreshDataArrow = () => {
     return (
-        <Col xs="12" className="d-flex justify-content-center mt-2 mb-5">
+        <Col xs="12" className="d-flex justify-content-center mt-2 mb-5 refresh-icon">
             <FontAwesomeIcon icon={faSync} size="5x" />
         </Col>
     )
