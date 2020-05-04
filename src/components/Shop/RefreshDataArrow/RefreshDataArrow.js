5320278047472c8b222cf0ad9be60b676f688bb0
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-04 12:10:25 +0300

    Create RefreshDataArrow.js

---
+++ src/components/Shop/RefreshDataArrow/RefreshDataArrow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
+
+const RefreshDataArrow = () => {
+    return (
+        <div>
+            <FontAwesomeIcon icon={faSync} />
+        </div>
+    )
+}
+
+export default RefreshDataArrow;
