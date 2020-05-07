c7dd6b502641009c27bc0a06332b8edaabe591c2
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-07 14:57:08 +0300

    Update CatalogController.js
    First realise update()

--- server/controllers/CatalogController.js
+++ server/controllers/CatalogController.js
@@ -1,28 +1,55 @@
-const CatalogModel = require('../db/models/CatalogModel');
+const CatalogModel = require("../db/models/CatalogModel");
 
 const CatalogController = {
-    all: async (req, res) => {
-        const categories = await CatalogModel.find();
-        res.json(categories);
-    },
+  all: async (req, res) => {
+    const categories = await CatalogModel.find()
+      .populate("categories")
+      .populate("beers")
+      .populate("parentCategory");
+    res.json(categories);
+  },
 
-    findById: async (req, res) => {
+  getById: async (req, res) => {
+    const category = await this.findCategoryById(req.params.id);
+    res.json(category);
+  },
 
-    },
+  create: async (req, res) => {
+    const newCategory = new CatalogModel(req.body);
+    const savedNewCategory = await newCategory.save();
+    res.json(savedNewCategory);
+  },
 
-    create: async (req, res) => {
-        const newCategory = new CatalogModel(req.body);
-        const savedNewCategory = await newCategory.save();
-        res.json(savedNewCategory);
-    },
+  update: async (req, res) => {
 
-    update: async (req, res) => {
+    //TODO data validation
+    const update = {
+      title: req.body.title,
+      categories: [...req.body.categories],
+      parentCategoryId: req.body.parentCategoryId
+    };
 
-    },
+    CatalogModel.findByIdAndUpdate({ _id: req.params.id }, update, (err, c) => {
+      if (c != null) {
+        CatalogModel.update(
+          { _id: update.parentCategoryId },
+          { $push: { categories: c._id } },
+          (err, raw) => {
+            res.json(raw);
+          }
+        );
+      }
+    });
+  },
 
-    delete: async (req, res) => {
+  delete: async (req, res) => {},
 
-    }
-}
+  findCategoryById: async (id) => {
+    return await CatalogModel.findById({ _id: id })
+      .populate("categories")
+      .populate("beers")
+      .populate("parentCategory");
+  },
+};
 
 module.exports = CatalogController;
