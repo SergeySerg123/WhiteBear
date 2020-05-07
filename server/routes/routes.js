9209283e43464e05ed71b9c2f175b4e7c105c518
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-07 14:57:30 +0300

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const BeerController = require('../controllers/BeerController');
+const CatalogController = require('../controllers/CatalogController');
 
-router.get('/api/beers/all', BeerController.all);
+//BeerController.js routes
+router.get('/api/beers', BeerController.all);
 router.get('/api/beers/:id', BeerController.getById);
-router.post('/api/beers/create', BeerController.create);
+router.post('/api/beers', BeerController.create);
 router.delete('/api/beers/:id', BeerController.delete);
+
+//CatalogController.js routes
+router.get('/api/catalog', CatalogController.all);
+router.get('/api/catalog/:id', CatalogController.getById);
+router.put('/api/catalog/:id', CatalogController.update);
+router.post('/api/catalog', CatalogController.create);
+router.delete('/api/catalog/:id', CatalogController.delete);
 
 module.exports = router;
